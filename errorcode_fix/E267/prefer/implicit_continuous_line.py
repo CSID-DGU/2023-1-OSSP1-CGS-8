bc1d6d936a0b3df50f363519cda67181a8d454ef
prefer52 <tjsgh151@naver.com>
2023-05-16 15:44:17 +0900

    Udpate: implicit_continuous_line/주석 및 \인식 개선

--- errorcode_fix/E267/prefer/implicit_continuous_line.py
+++ errorcode_fix/E267/prefer/implicit_continuous_line.py
@@ -5,19 +5,28 @@ def implicit_continuous_line(self, result):
     comment = target[offset:].strip()
     target = target[:offset].rstrip()
     
+    # Get the number of the line currently reading
     current_line = line_index
     first_line = self.source[current_line][:]
+    # Compile implicit lines by replacing them with one line
     while current_line >= 0:
         current_line -= 1
-        first_line += self.source[current_line][:-1]
+        # Remove '\' and add lines if '\' is included
+        if self.source[current_line][-1] == chr(92):
+            first_line += self.source[current_line][:-1]
+        else:
+            first_line += self.source[current_line][:]
         try:
             compile(first_line, '<string>', 'single')
         except (SyntaxError, TypeError, ValueError):
+            # Escape loop for other lines not compiled
             break
     
+    # Find the first line number that was compiled successfully
     if current_line < 0: current_line = 0
     else: current_line += 1
     
+    # Match the indent level of the first line with the comment
     indent_word = _get_indentation(self.source[current_line])
     comment = indent_word + comment + '\n'
     self.source[current_line] = comment + self.source[result['line'] - 1]
