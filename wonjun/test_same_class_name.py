f96def92584d9ed9fb692bf8f7910f5f638720d7
jun6292 <c68254@gmail.com>
2023-05-17 19:18:48 +0900

    Add: test_same_class_name.py

---
+++ wonjun/test_same_class_name.py
@@ -0,0 +1,16 @@
+# __author__ = ('Wonjun Jo <c68254@gmail.com>')
+
+class SameClass:
+    def SC():
+        return None
+    
+class same_class:
+    def s_c():
+        return None
+    
+class sameClass:
+    def sC():
+        return None
+
+
+# python3 autopep8.py --aggressive --aggressive --aggressive test_same_class_name.py
