a08904c6c02c6890f0ba38501a97ff6a0d8241b0
jun6292 <c68254@gmail.com>
2023-05-17 19:20:40 +0900

    Add: test_same_function_name.py

---
+++ wonjun/test_same_function_name.py
@@ -0,0 +1,12 @@
+# __author__ = ('Wonjun Jo <c68254@gmail.com>')
+
+def same_name():
+    return None
+
+def SameName():
+    return None
+
+def sameName():
+    return None
+
+# python3 autopep8.py --aggressive --aggressive --aggressive test_same_function_name.py
