57a6483b94d5e255185648aceb237f9746866143
jun6292 <c68254@gmail.com>
2023-05-18 21:37:47 +0900

    Update: test_W701.py

--- wonjun/test_W701.py
+++ wonjun/test_W701.py
@@ -24,4 +24,20 @@ class Camelcased:
     def d():
         return None
     
+class Ab:
+    def e():
+        return None
+    
+class AB:
+    def f():
+        return None
+    
+class AbC:
+    def g():
+        return None
+    
+class AbCd:
+    def h():
+        return None
+
 # python3 autopep8.py --aggressive --aggressive --aggressive test_W701.py
