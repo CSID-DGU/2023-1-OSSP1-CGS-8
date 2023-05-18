7ba405e048fb0b49c5a185d96cae2f4b85858190
Chajaesik01 <ckwotlr@dongguk.edu>
2023-05-18 22:30:11 +0900

    Fix: E268/인라인 주석 위치 오류 수정, 백슬래쉬 삭제 구현

--- errorcode_fix/E267/chajaesik/Edit_inline_comments.py
+++ errorcode_fix/E267/chajaesik/Edit_inline_comments.py
@@ -1,7 +1,6 @@
 #고려사항 1번
 import time
 
-start = time.time()
 def fix_e267(self, result):
     """inline comment are not recommended"""
     start = time.time()
@@ -26,61 +25,63 @@ def fix_e267(self, result):
 
 
 
-#고려사항 2,3,4 
+
+
+# 고려사항 1,2,3,4 
 def fix_e268(self, result):
     """inline comment are not recommended"""
     line_index = result['line'] - 1
-    target = self.source[line_index] 
+    target = self.source[line_index][:] # [:] 복사해서 반환해주는 
     offset = result['column'] - 1 
-    #code = target[:offset].rstrip(' \t#') 
     # 주석 내용 저장
-    comment = target[offset:].lstrip(' \t#')
-    while(check_parentheses(target) is False):
-        line_index = result['line'] - 1
-        target = self.source[line_index] 
-        line = result['line'] - 2
-        self.source[line_index] = self.source[target] + self.source[line] #해당 줄과 밑의 줄을 합친다
-        self.source[line] = ''   #밑줄을 공백으로
+    comment = target[offset+1:].lstrip()
+    count = 0
+    #target1에 해당 코드 줄 내용 저장
+    target1= self.source[target][:]
+    while not check_count_parenthesis(target1, count):
+        target1 = self.source[line_index][:] #해당 줄의 내용 저장
+        line_index -= 1   #줄의 인덱스를 위로 올리고
+        target2 = self.source[line_index][:] #올라간 인덱스의 줄 , 즉 윗줄 내용복사
+        target1 = target1 + target2 #오류 발생한 줄과 그 윗줄의 내용을 합친다
+    # 들여쓰기 수준 맞추기
+    indent = _get_indentation(line_index)
     #윗줄에 주석 추가
-    line = result['line'] - 2
-    self.source['line'] =  comment
+    self.source[line_index] =  indent + '# ' + comment
+
 
 # 괄호의 개수로 체크하는 함수
-def check_parentheses(expression):
+# 작동 확인
+def check_count_parenthesis(expression, count):
     """Check the number of parentheses """
-    count = 0
-
     for char in expression:
         if char == '(' or char =='[' or char =='{':
             count += 1
         elif char == ')' or char ==']' or char =='}':
             count -= 1
+    count += expression.count('(') + expression.count('[') + expression.count('{')
+    count -= expression.count(")") + expression.count(']') + expression.count('}')
+    if count < 0:
+        return False
+    return True
 
-        if count < 0:
-            return False
-    return count == 0
-#괄호 쌍이 일치하지 않는 경우
-#여는 괄호와 닫는 괄호의 순서가 잘못된 경우 오류 발생
-                    
+print(check_count_parenthesis("([a+b+c[abcde]])",0))
 
-'''  
+
+'''
 고려사항 1
 a = 1 + 2 + 3 + 4 + 5  \
     + 5 + 6 + 7 + 8 + 9 \
-    + 10 + 11 + 12
-
+    + 10 + 11 + 12 #asdfawef
     
-고려사항 2
-if (a and b and
-    c.getResult(a, b, c, d
-    e, f, g, h)
-    ) #이런이런 함수
+#인라인 주석
+if (a and b andc.getResult(a, b, c, de, f, g, h))
     
 고려사항 3
-if (a and b and
-    c.getResult(a, b, c, d
-                e, f, g, h)) #이런이런 함수
-                
+    # 이런저런 함수
+    if (a and b and
+        c.getResult(a, b, c, d
+                    e, f, g, h)) #이런이런 함수
+    
 고려사항 4
 () {} []가 섞여 있을 때
 '''
