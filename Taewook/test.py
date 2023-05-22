d8c0c65c46abe76cba27b38f479a68bf5008dcfd
taewook <davis0625@dongguk.edu>
2023-05-22 22:58:57 +0900

    FIX : test.py / snake_case 부분 확인 가능하게 변경

--- Taewook/test.py
+++ Taewook/test.py
@@ -1,13 +1,13 @@
-class rectangle:
+class Rectangle:
     count = 0  # 클래스 변수
 
     # 초기자(initializer)
     def __init__(self, width, height):  # self.* : 인스턴스변수
         self.width = width
         self.height = height
-        rectangle.count += 1
+        Rectangle.count += 1
 
     # 메서드
-    def CalcArea(self):
+    def cal__carea(self):
         area = self.width * self.height
         return area
