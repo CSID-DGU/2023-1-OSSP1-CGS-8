6635f3ed347f7b91e45ece42719197fc2da162d9
prefer52 <tjsgh151@naver.com>
2023-05-22 10:47:09 +0900

    Udpate: implicit_continuous_line/암시적 줄 잇기 괄호 개수 인식 구

--- errorcode_fix/E267/test.py
+++ errorcode_fix/E267/test.py
@@ -13,23 +13,26 @@ def fix_test(self, result):
     comment = target[offset:].strip()
     self.source[line_index] = target[:offset].rstrip()
     
-    # 주석이 등장한 마지막 라인을 저장
-    one_line = self.source[line_index][:]
-    # 암시적으로 이어지는 줄을 하나의 줄로 합쳐서 컴파일
+    open_bracket, close_bracket = 0, 0
     while line_index >= 0:
-        line_index -= 1
-        # 만약 '\'가 포함되어있다면 '\' 제거 후 줄 합치기
-        if self.source[line_index][-1] == chr(92):
-            one_line = self.source[line_index][:-1] + one_line
-        else:
-            one_line = self.source[line_index][:] + one_line
-        try:
-            compile(one_line, '<string>', 'single')
-            # 암시적 줄 잇기가 시작되는 줄을 만나면
-            # 정상적으로 컴파일되므로 루프 탈출
-            break
-        except (SyntaxError, TypeError, ValueError):
+        current_line = self.source[line_index]
+        
+        open_bracket += current_line.count('(')\
+            + current_line.count('{')\
+            + current_line.count('[')
+        close_bracket += current_line.count(')')\
+            + current_line.count('}')\
+            + current_line.count(']')
+        
+        if (line_index-1 >= 0 and
+                self.source[line_index-1][-1] == chr(92)):
+            line_index -= 1
             continue
+        
+        if (open_bracket == close_bracket):
+            break
+        line_index -= 1
+        
     # 암시적 줄 잇기가 시작되는 줄의 들여쓰기와 인라인 주석의
     # 들여쓰기 수준 맞추기
     indent_word = _get_indentation(self.source[line_index])
@@ -38,11 +41,11 @@ def fix_test(self, result):
 
 
 result = {}
-result['line'] = 3
-result['column'] = 23
+result['line'] = 4
+result['column'] = 29
 def self(): pass
-self.source = ['    a = 1 + 2 + 3 + 4 + 5 \\', '        + 5 + 6 + 7 + 8 + 9 \\',
-               '        + 10 + 11 + 12 # 하하핳하']
+self.source = ['    if (a and b and', '        c.getResult(1 + 2 + 3\\',
+               '            + 4 + 5 + 6\\', '                + 7) == 10): #이런이런 함수']
 
 fix_test(self, result)
 print('\n'.join(self.source))
