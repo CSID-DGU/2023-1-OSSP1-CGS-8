c4719e039bb701dac550c90bc09afa54d3ae3f8e
jun6292 <c68254@gmail.com>
2023-05-24 14:15:01 +0900

    Fix: W7 test file/작명 컨벤션 테스트 케이스 수정

--- wonjun/test_W701.py
+++ wonjun/test_W701.py
@@ -1,43 +1,45 @@
 # __author__ = ('Wonjun Jo <c68254@gmail.com>')
+# 작명 컨벤션: 클래스 이름 변경
+# 클래스 이름은 CapWords 컨벤션을 따른다.
 
-class CamelCase:
+# 변경 X: CapWords 컨벤션 만족
+class CapWords:
     def a():
         return None
     
-
-class camelCasea:    
+# CamelCase로 변경
+class camelCase:    
     def a():
         return None
     
-
-class camelcaseb:
+# Noncapwords로 변경
+class noncapwords:
     def b():
         return None
 
-
-class camel_casec:
+# MixedCase로 변경
+class mixed_case:
     def c():
         return None
 
-
-class Camelcased:
+# 변경 X, 약어를 모두 대문자로 사용하는 것이 더 좋다고 PEP8에 명시
+class HTTPServerError:
     def d():
         return None
-    
-class Ab:
+
+# 변경 X
+class A:
     def e():
         return None
     
-class AB:
-    def f():
-        return None
-    
-class AbC:
+# MixedCase2로 변경 --> 아직 구현 단계
+class Mixed_Case2:
     def g():
         return None
-    
+
+# 변경 X, Capwords 컨벤션 만족
 class AbCd:
     def h():
         return None
-
+    
 # python3 autopep8.py --aggressive --aggressive --aggressive test_W701.py
--- wonjun/test_W702.py
+++ wonjun/test_W702.py
@@ -1,22 +1,29 @@
 # __author__ = ('Wonjun Jo <c68254@gmail.com>')
+# 작명 컨벤션: 함수 이름 변경
+# 함수 이름은 소문자여야 하며 가독성을 위해 _로 구분되어야 한다. (스네이크 케이스)
 
+# 변경 X: 스네이크 케이스 컨벤션 만족
 def snake_case():
     return None
 
-def SnakeCasea():
+# cap_words로 변경
+def CapWords():
     return None
 
-def snakeCaseb():
+# camel_case로 변경
+def camelCase():
     return None
 
-def snakecasec():
+# 변경 X: 함수명이 모두 소문자
+def snakecaselower():
     return None
 
-# 추가로 고려해봐야 할 케이스
-def Snake_Cased():
+# mixed_case로 변경 --> 구현 중
+def Mixed_Case():
     return None
 
-def snake__casee():
+# mixed_case2로 변경 --> 구현 중
+def mixed_Case2():
     return None
 
 # python3 autopep8.py --aggressive --aggressive --aggressive test_W702.py
