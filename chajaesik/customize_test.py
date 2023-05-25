d56d2ede718ec58476feb65ea420463d11620791
Chajaesik01 <ckwotlr@dongguk.edu>
2023-05-25 11:06:23 +0900

    Update : customize_test/sing_quote를 double_quote로 변환 testcase추가 및 \로 암시적 줄읽기시 공백 발생 제거 기능 추가

--- chajaesik/customize_test.py
+++ chajaesik/customize_test.py
@@ -27,11 +27,28 @@ def fix_test(self, result):
     fix = replace_quotes(target)
     self.source[line_index] = fix
 
+
+
+''' 암시적 줄 읽기의 공백을 삭제해야 한다면 .. 
+target = replace_quotes(self.source[line_index])
+    offset = result['column']-1
+    comment = target[offset:].strip()
+    self.source[line_index] = target[:offset].rstrip()
+    #print(self.source[line_index]+comment)
+    self.source[line_index] = self.source[line_index]+comment '''
+
 result = {}
 result['line'] = 0
 result['column'] = 16
 def self(): pass
 
-self.source = ["해당 줄에서 '''이 연속으로 3개 나오는 경우를 찾는다"]
+
+
+self.source =  ["'''해당 줄에서 \
+               이 연속으로 3개 \
+               나오는 경우를 찾는다'''"]
+
+
+
 fix_test(self, result)
 print('\n'.join(self.source))
