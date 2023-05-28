0bba2f467647de958e347111241aa2fa9eabb215
Chajaesik01 <ckwotlr@dongguk.edu>
2023-05-28 12:29:34 +0900

    Update:customize_test/ E744, E745추가, autopep8에 Customize.get_attribute 추가

--- chajaesik/customize_test.py
+++ chajaesik/customize_test.py
@@ -8,7 +8,20 @@ def _get_indentation(line):
 
     return ''
 
-def replace_quotes(text):
+def make_single_quotes(text):
+    result=''
+    i = 0
+    while i < len(text):
+        if text[i:i+3] == '"""':
+            result += "'''"
+            i += 3
+        else:
+            result += text[i]
+            i += 1
+    return result
+
+
+def make_double_quotes(text):
     result = ''
     i = 0
     while i < len(text):
@@ -21,21 +34,19 @@ def replace_quotes(text):
     return result
     
     
-def fix_test(self, result):
+def fix_E744(self, result):
     line_index = result['line'] - 1
     target = self.source[line_index]
-    fix = replace_quotes(target)
+    fix = make_double_quotes(target)
+    self.source[line_index] = fix
+
+def fix_E745(self,result):
+    line_index = result['line'] - 1
+    target = self.source[line_index]
+    fix = make_single_quotes(target)
     self.source[line_index] = fix
 
 
-
-''' 암시적 줄 읽기의 공백을 삭제해야 한다면 .. 
-target = replace_quotes(self.source[line_index])
-    offset = result['column']-1
-    comment = target[offset:].strip()
-    self.source[line_index] = target[:offset].rstrip()
-    #print(self.source[line_index]+comment)
-    self.source[line_index] = self.source[line_index]+comment '''
 
 result = {}
 result['line'] = 0
@@ -43,12 +54,16 @@ result['column'] = 16
 def self(): pass
 
 
-
+#test case
 self.source =  ["'''해당 줄에서 \
                이 연속으로 3개 \
                나오는 경우를 찾는다'''"]
 
 
-
-fix_test(self, result)
+fix_E744(self, result)
+print('\n'.join(self.source))
+self.source =  ['"""해당 줄에서 \
+               이 연속으로 3개 \
+               나오는 경우를 찾는다"""']
+fix_E745(self,result)
 print('\n'.join(self.source))
