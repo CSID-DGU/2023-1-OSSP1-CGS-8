81abfe7de363018dbdd2a3c5d1bf425784d0f600
taewook <davis0625@dongguk.edu>
2023-05-31 01:58:09 +0900

    Fix: test.py / 주석에 class 있는 테스트케이스로 수정

--- Taewook/test.py
+++ Taewook/test.py
@@ -1,13 +1,13 @@
-class rectangle:
+class rec_tangle:
     count = 0
 
     # 초기자(initializer)
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        Rectangle.count += 1
+        rec_tangle.count += 1
 
-    # 메서드
+    # 메서드 class taewook
     '''계산하는 함수'''
     def Calcarea(self):
         ''' 계산하는 함수를 나타낸다
