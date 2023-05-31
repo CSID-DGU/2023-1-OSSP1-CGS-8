4d7eed42ad412c770cec3b2daa88971d66f2c465
taewook <davis0625@dongguk.edu>
2023-05-31 11:56:48 +0900

    FIX: test.py / 테스트 케이스 변경

--- Taewook/test.py
+++ Taewook/test.py
@@ -1,16 +1,7 @@
-class rec_tangle:
-    count = 0
-
-    # 초기자(initializer)
+class rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        rec_tangle.count += 1
 
-    # 메서드 class taewook
-    '''계산하는 함수'''
-    def Calcarea(self):
-        ''' 계산하는 함수를 나타낸다
-        '''
-        area = self.width*self.height + self.width*self.height
-        return area
+    def area(self):
+        return self.width * self.height
