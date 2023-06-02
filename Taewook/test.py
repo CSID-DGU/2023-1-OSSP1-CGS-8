d82ddd99dffd25955de875ad2dd9a259aa577c85
taewook <127061738+Taew00k@users.noreply.github.com>
2023-06-02 15:17:06 +0900

    Delete test.py

--- Taewook/test.py
+++
@@ -1,8 +0,0 @@
-class rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-# class rectangle
-    def Area(self):
-        return self.width * self.height
